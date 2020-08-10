603e58482265f469996e654d7e95d759a1157acf
Saem Jeon <sj846@drexel.edu>
2020-08-09 23:22:48 -0400

    Quick Sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -2,6 +2,7 @@ import random
 import copy
 from insertion_sort import *
 from merge_sort import *
+from quick_sort import *
 
 if __name__ == "__main__":
   arr = [random.randint(0, 100) for i in range(10)]
@@ -9,3 +10,5 @@ if __name__ == "__main__":
   print(insertion_sort(copy.deepcopy(arr)))
   print(arr)
   print(merge_sort(copy.deepcopy(arr)))
+  print(arr)
+  print(quick_sort(copy.deepcopy(arr), 0, len(arr)-1))
